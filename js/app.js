e39224e703e738229f4ed156398ebd2b4050831f
= <=>
2016-10-26 00:08:17 -0500

    Add a dynamically expanding key value list

--- js/app.js
+++ js/app.js
@@ -33,6 +33,42 @@ Vue.component('function-instance', {
 })
 
 
+Vue.component('key-value-list', {
+    template: "#tmpl-key-val", 
+    data: function() {
+        return {
+            "items": [
+                {"key": "item 1", "value": "val 1"},
+                {"key": "item 2", "value": "val 2"},
+            ],
+            "newkey": "",
+            "newvalue": ""
+        }
+    },
+    methods: {
+        addAndFocus: function(selector) {
+            this.items.push({"key": this.newkey, "value": this.newvalue})
+            
+            // 
+            this.newkey = "";
+            this.newvalue = "";
+            
+            Vue.nextTick(function () {
+                let valueInputs = document.querySelectorAll(selector);
+                valueInputs[valueInputs.length - 1].focus();
+            });
+
+        },
+        addNewKey: function(evnt) {
+            this.addAndFocus(".inKey");
+        },
+        addNewValue: function(evnt) {
+            this.addAndFocus(".inVal");
+            
+        }
+    }
+})
+
 TYPES = ["String", "Number", "Boolean", "Function", "Object", "Array", "Reference"]
 
 window.fDefs = [
@@ -51,7 +87,7 @@ window.fDefs = [
     },
     {
         "id": 1,
-        "name": "Add",
+        "name": "Multiply",
         "parameters": [
             {
                 "name": "a",
@@ -64,7 +100,7 @@ window.fDefs = [
     ], 
      func: function(args) {
          console.log(args);
-         return args["a"] + args["b"];
+         return args["a"] * args["b"];
      }
     },
 ];
@@ -84,7 +120,7 @@ var app = new Vue({
         toggleInstanceEdit(index) {
             console.log(this.instances[index]);
             this.instances[index].isEdit = !this.instances[index].isEdit;
-        }, 
+        },
         showAddInstance() {
             console.log("show add instance");
             console.log(this.newFuncId);
@@ -93,7 +129,7 @@ var app = new Vue({
                 "fDef": this.newFuncId,
                 "name": "New Function",
                 "args": {},
-                "isEdit": false
+                "isEdit": true
             }
             instances.push(newInstance);
             // this.instances.push(newInstance);
