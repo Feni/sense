284fc125c411d9d5d76e1567e59900f280e01e79
= <=>
2016-10-27 00:07:16 -0500

    Index the graph nodes by relation types to optimize lookup

--- engine.py
+++ engine.py
@@ -5,32 +5,33 @@ class Engine:
         # Stores a node name to list of relations.
         # Nodes are constant names in this version. Have a different layer to map human readable names to these constants.
         # TODO: The graph should store not a list, but a map of relationType to list.
-        self.graph = defaultdict(list)
+        self.graph = defaultdict(lambda : defaultdict(list))
+
+    def inverse_relation(self, relation):
+        if "-" in relation:
+            return relation.replace("-", "")
+        else:
+            return "-" + relation
 
     def statement(self, statement_graph):
         for node, relations in statement_graph.items():
-            self.graph[node] += relations
-            # Add reverse relations
-            for relation, related_node in relations:
-                self.graph[related_node].append( ("reverse_" + relation, node))
+            for relation, related_nodes in relations.items():
+                for related_node in related_nodes:
+                    self.graph[node][relation].append(related_node)
+                    self.graph[related_node][self.inverse_relation(relation)].append(node)
 
     def query(self, query_graph):
         response = []
         for node, relations in query_graph.items():
-            if node == "UNKNOWN":
-                # Backwards relation
-                for relation, relation_node in relations:
-                    assert relation_node != "UNKNOWN"
-                    return filter(lambda x: x[0] == "reverse_" + relation, self.graph[relation_node])
-            else:
-                # Forward lookup
-                node_relations = self.graph[node]
-                for relation, relation_node in relations:
-                    assert relation_node == "UNKNOWN"
-                    # Then this is what we have to find.
-                    return filter(lambda x: x[0] == relation, node_relations)
-
-
+            for relation, related_nodes in relations.items():
+                for related_node in related_nodes:
+                    if node == "UNKNOWN":
+                        # Work backwards from the relations
+                        assert related_node != "UNKNOWN"
+                        return self.graph[related_node][self.inverse_relation(relation)]
+                    elif related_node == "UNKNOWN":
+                        # Forward lookup
+                        return self.graph[node][relation]
 
     def list_all(self):
         print self.graph
--- test_engine.py
+++ test_engine.py
@@ -2,11 +2,11 @@ from engine import Engine
 
 def test_variables():
     engine = Engine()
-    engine.statement({"dog": [("is", "animal")]})
-    engine.statement({"dog": [("is", "mammal")]})
+    engine.statement({"dog": {"is": ["animal"]}})
+    engine.statement({"dog": {"is": ["mammal"]}})
     engine.list_all()
-    print engine.query({"dog": [("is", "UNKNOWN")]})
-    print engine.query({"UNKNOWN": [("is", "mammal")]})
+    print engine.query({"dog": {"is": ["UNKNOWN"]}})
+    print engine.query({"UNKNOWN": {"is": ["mammal"]} })
     # ref = engine.define_value(42, alias="hello world")
     # print ref
     # assert engine.get_memory(ref) == 42
