f2a02c518c94d63002d813d47d584fa74d5750e4
= <=>
2016-10-26 23:52:53 -0500

    Graph based statement and query engine v1

--- engine.py
+++ engine.py
@@ -1,22 +1,39 @@
-import uuid
+from collections import defaultdict, namedtuple
 
 class Engine:
     def __init__(self):
-        # Maps a modifyable, human readable name to a constant string identifier.
-        self.namespace = {}
-        self.memory = {}
-        # The memory stores a "map" of how to find the value - in memory, on disk, on network, etc. with all relevant details.
+        # Stores a node name to list of relations.
+        # Nodes are constant names in this version. Have a different layer to map human readable names to these constants.
+        # TODO: The graph should store not a list, but a map of relationType to list.
+        self.graph = defaultdict(list)
 
-    def define_value(self, value, alias=None):
-        # Define a value and asigns it an optional alias. 
-        reference = str(uuid.uuid4())
-        if reference not in self.memory and value:
-            self.memory[reference] = value
-        self.namespace[alias] = reference
-        return reference
+    def statement(self, statement_graph):
+        for node, relations in statement_graph.items():
+            self.graph[node] += relations
+            # Add reverse relations
+            for relation, related_node in relations:
+                self.graph[related_node].append( ("reverse_" + relation, node))
 
-    def get_memory(self, reference):
-        return self.memory[reference]
+    def query(self, query_graph):
+        response = []
+        for node, relations in query_graph.items():
+            if node == "UNKNOWN":
+                # Backwards relation
+                for relation, relation_node in relations:
+                    assert relation_node != "UNKNOWN"
+                    return filter(lambda x: x[0] == "reverse_" + relation, self.graph[relation_node])
+            else:
+                # Forward lookup
+                node_relations = self.graph[node]
+                for relation, relation_node in relations:
+                    assert relation_node == "UNKNOWN"
+                    # Then this is what we have to find.
+                    return filter(lambda x: x[0] == relation, node_relations)
 
-    # All variables are immutable.
+
+
+    def list_all(self):
+        print self.graph
+
+    
         
