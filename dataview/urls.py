738c201b4687371a7ddd5b87c186fad85a237e78
= <=>
2016-11-20 00:30:24 -0600

    Got a basic django server running

---
+++ dataview/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+from django.contrib import admin
+from .views import *
+
+
+#2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+
+urlpatterns = [
+    url(r'^$', home),
+]
