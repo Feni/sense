5f7891ab14f698206919cdab8fbb501345c2de37
= <=>
2016-11-23 00:58:19 -0600

    template cleanup to remove some duplicated code

--- dataview/urls.py
+++ dataview/urls.py
@@ -4,9 +4,12 @@ from .views import *
 
 #2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
 
+# Views should be nouns
+# Forms and api endpoints should be verbs
+
 urlpatterns = [
     url(r'^$', CollectionListView.as_view()),
     url(r'^collections/(?P<pk>[-\w]+)/$', CollectionDetailView.as_view()),
-    url(r'^new$', CollectionEntry.as_view())
+    url(r'^collections/(?P<collection_id>[-\w]+)/create$', CollectionEntry.as_view())
 ]
 
--- dataview/views.py
+++ dataview/views.py
@@ -8,26 +8,37 @@ from .models import *
 from .forms import *
 
 
-class CollectionsContext(ContextMixin):
+class CollectionsListContext(ContextMixin):
     def get_collections(self):
         return Collections.objects.all()[:10] # TODO: Limit to user
 
     def get_context_data(self, **kwargs):
-        context = super(CollectionsContext, self).get_context_data(**kwargs)
+        context = super(CollectionsListContext, self).get_context_data(**kwargs)
         context["collections_list"] = self.get_collections()
+        return context
+
+class CollectionContext(ContextMixin):
+    def get_collection(self):
+        if not hasattr(self, "_collection"):
+            self._collection = Collections.objects.get(id=self.kwargs["collection_id"])
+        return self._collection
+
+    def get_context_data(self, **kwargs):
+        context = super(CollectionContext, self).get_context_data(**kwargs)
+        context["collection"] = self.get_collection()
         return context
 
 class CollectionListView(ListView):
     model = Collections
 
-class CollectionDetailView(CollectionsContext, DetailView):
+class CollectionDetailView(CollectionsListContext, DetailView):
     model = Collections
 
-class CollectionEntry(CollectionsContext, FormView):
+class CollectionEntry(CollectionsListContext, CollectionContext, FormView):
     template_name = "dataview/collectionentry_form.html"
 
     def get_fields(self):
-        json_fields = list(Collections.objects.get(id=1).fields().values_list("field_name", flat=True))
+        json_fields = list(self.get_collection().fields().values_list("field_name", flat=True))
         return json_fields
 
     def get_form(self, form_class=None):
@@ -41,7 +52,7 @@ class CollectionEntry(CollectionsContext, FormView):
                 field_id = int(field.replace("json_", ""))
                 field_name = fields[field_id]
                 values[field_name] = form.cleaned_data[field]
-        collection = Collections.objects.get(id=1)
+        collection = self.get_collection()
         max_row_id = Dataset.max_id(collection)
         newrow = Dataset(row_id=max_row_id + 1, row=values, collection=collection)
         newrow.save()
